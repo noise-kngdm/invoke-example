ebc099fef8ace05340ccc2c155f07f38fbfdf86d
Gonzalo Abril Paniza <nyrsleep@protonmail.com>
2024-06-14 10:16:00 +0200

    Add improvements and formatting to invoke tasks

--- tasks.py
+++ tasks.py
@@ -39,31 +39,34 @@ from pathlib import Path
 from invoke import task, Responder
 from invoke.exceptions import Failure
 
-DJANGO_EXEC = 'docker compose exec django'
+DJANGO_EXEC = "docker compose exec django"
 
 
 @task
 def down(ctx):
     """Stop all the services"""
-    ctx.run('docker compose down -t0 -v --remove-orphans')
+    ctx.run("docker compose down -t0 -v --remove-orphans")
 
 
-@task(help={'all': 'Use the default docker file instead of the dev one'})
-def up(ctx, all=False):
+@task
+def up(ctx):
     """Create and start the containers"""
-    if all:
-        ctx.run('docker compose up -d')
-    else:
-        ctx.run(f'docker compose -f {ctx.DEV_DOCKER_COMPOSE} up -d')
+    ctx.run(f"docker compose --profile debug -f {ctx.DEV_DOCKER_COMPOSE} up -d")
 
 
-@task(help={'command': 'Command that should be runned by Django',
-            'watchers': 'Custom watchers that should be used by invoke'})
-def command(ctx, command='', watchers=None):
+@task(
+    help={
+        "command": "Command that should be runned by Django",
+        "watchers": "Custom watchers that should be used by invoke",
+    }
+)
+def command(ctx, command="", watchers=None, executor="/entrypoint.sh"):
     """Run a django command"""
+    if executor is None:
+        executor = "python"
     if watchers is None:
         watchers = []
-    ctx.run(f"{DJANGO_EXEC} python manage.py {command}", pty=True, watchers=watchers)
+    ctx.run(f"{DJANGO_EXEC} {executor} ./manage.py {command}", pty=True, watchers=watchers)
 
 
 def unapplied_migrations(ctx) -> bool:
@@ -74,8 +77,8 @@ def unapplied_migrations(ctx) -> bool:
         bool: True if there are unapplied migrations, False otherwise.
     """
     try:
-        eso = ctx.run(f"{DJANGO_EXEC} bash -c 'python manage.py showmigrations'", hide='both')
-        if '[ ]' in eso.stdout:
+        eso = ctx.run(f"{DJANGO_EXEC} bash -c 'python manage.py showmigrations'", hide="both")
+        if "[ ]" in eso.stdout:
             return True
     except Exception as e:
         print(f"Error showing unapplied migrations: {e}")
@@ -87,30 +90,51 @@ def unapplied_migrations(ctx) -> bool:
 def checkmigrations(ctx):
     """Check if there are migrations to be applied"""
     if unapplied_migrations(ctx):
-        print('There are unapplied migrations')
+        print("There are unapplied migrations")
     else:
-        print('All migrations have been applied')
+        print("All migrations have been applied")
 
 
-@task
-def db(ctx, skip_db_wipe=False):
+@task(
+    help={
+        "patient_count": "Number of patients that should be created",
+        "crm_data_day_count": "Number of days of crm data that should be created",
+        "rpm_data_day_count": "Number of days of rpm data that should be created",
+        "skip_db_wipe": "If the database should be wiped before seeding",
+        "keep_base_data": "If the base data should be kept when wiping the database",
+        "profile": "If the command should be profiled",
+    }
+)
+def db(
+    ctx,
+    patient_count=40,
+    crm_data_day_count=100,
+    rpm_data_day_count=100,
+    skip_db_wipe=False,
+    keep_base_data=False,
+    profile=False,
+):
     """Reset and fill the db with sample data"""
     while unapplied_migrations(ctx):
         print("There are unapplied migrations, sleeping...")
         time.sleep(ctx.SECONDS_SLEEP)
-
     responder_y_n = Responder(
-        pattern='This command will seed new data\(and optionally wipe the database\)\. Continue\? \(Y/N\)',
-        response='y\n'
+        pattern="This command will seed new data\(and optionally wipe the database\)\. Continue\? \(Y/N\)",
+        response="y\n",
     )
-
-    parsed_args = ""
+    parsed_args = f"--patient_count {patient_count} --crm_data_day_count {crm_data_day_count} --rpm_data_day_count {rpm_data_day_count}"
     if skip_db_wipe:
-        parsed_args += ' --skip_db_wipe'
+        parsed_args += " --skip_db_wipe"
+    if keep_base_data:
+        parsed_args += " --keep_base_data"
+    if profile:
+        parsed_args += " --profile"
 
     while True:
         try:
-            command(ctx, command=f'refresh_db {parsed_args}', watchers=[responder_y_n])
+            command(
+                ctx, command=f"refresh_db {parsed_args} --confirm", executor="/entrypoint.sh", watchers=[responder_y_n]
+            )
         except KeyboardInterrupt:
             return
         except Failure:
@@ -119,81 +143,97 @@ def db(ctx, skip_db_wipe=False):
         break
 
 
-@task(help={'follow': 'If the command should continue streaming the new output from the container\'s STDOUT and STDERR',
-            'service': 'Service whose logs should be printed. Defaults to django.'})
-def logs(ctx, follow=False, service='django'):
+@task(
+    help={
+        "follow": "If the command should continue streaming the new output from the container's STDOUT and STDERR",
+        "service": "Service whose logs should be printed. Defaults to django.",
+    }
+)
+def logs(ctx, follow=False, service="django"):
     """Print the logs of a service"""
     ctx.run(f"docker compose logs {'--follow' if follow else ''} {service}", pty=True)
 
 
-@task(help={'no_cache': 'If the cache should be ignored when building the images'})
-def build(ctx, no_cache=False):
+@task(
+    help={
+        "no_cache": "If the cache should be ignored when building the images",
+        "service": "Service that should be built, comma-separated. Defaults to all services.",
+    }
+)
+def build(ctx, no_cache=False, service=""):
     """Build all the services"""
-    docker_args = ' '.join(['--' + flag for flag in ctx.DOCKER.BUILD_ARGS])
+    append = " ".join(service.split(",")) if service else ""
+    docker_args = " ".join(["--" + flag for flag in ctx.DOCKER.BUILD_ARGS])
     if no_cache:
-        docker_args += ' --no-cache'
-    ctx.run(f"docker compose -f {ctx.DEV_DOCKER_COMPOSE} build {docker_args}", pty=True)
+        docker_args += " --no-cache"
+    ctx.run(f"docker compose -f {ctx.DEV_DOCKER_COMPOSE} build {docker_args} {append}", pty=True)
 
 
 def tests_roots(tests):
     roots = []
-    for test in tests.split(','):
-        working_tests = test.replace('.', '/')
-        roots.append(working_tests.split('/')[0])
+    for test in tests.split(","):
+        working_tests = test.replace(".", "/")
+        roots.append(working_tests.split("/")[0])
     return roots
 
 
 def selected_tests(tests):
     selected_tests = []
-    for test in tests.split(','):
-        if '/' in test:
-            test = test.replace('/', '.')
-        if test.endswith('.py'):
-            test = test[: -3]
-        test = test.rstrip('.')
+    for test in tests.split(","):
+        if "/" in test:
+            test = test.replace("/", ".")
+        if test.endswith(".py"):
+            test = test[:-3]
+        test = test.rstrip(".")
 
         selected_tests.append(test)
     return selected_tests
 
 
-@task(help={
-    'test': 'Test that should be run. Specified with . or / as path delimiter. Select different tests with a comma. e.g. "inv test -t \'reports.tests.test_views,reports.tests.test_models\'"',
-    'keywords': 'Keywords that should be used to filter the tests.',
-    'coverage': 'If the coverage report should be displayed.'
-})
-def test(ctx, test, keywords='', coverage=False):
+@task(
+    help={
+        "test": "Test that should be run. Specified with . or / as path delimiter. Select different tests with a comma. e.g. \"inv test -t 'clinic.tests.test_views,clinic.tests.test_models'\"",
+        "keywords": "Keywords that should be used to filter the tests.",
+        "coverage": "If the coverage report should be displayed.",
+    }
+)
+def test(ctx, test, keywords="", coverage=False):
     """Run a unit test"""
     roots = tests_roots(test)
     tests = selected_tests(test)
-    test_flags = ' '.join(['--' + flag for flag in ctx.TEST.FLAGS])
+    test_flags = " ".join(["--" + flag for flag in ctx.TEST.FLAGS])
 
-    command_keywords = '' if not keywords else f'-k {keywords}'
+    command_keywords = "" if not keywords else f"-k {keywords}"
 
-    executer = f"coverage run --source='{','.join(roots)}'" if coverage else 'python'
+    executer = "coverage run" if coverage else "/entrypoint.sh"
 
-    command = f"{DJANGO_EXEC} {executer} manage.py test {test_flags} {' '.join(tests)} {command_keywords}"
+    command = f"{DJANGO_EXEC} {executer} ./manage.py test {test_flags} {' '.join(tests)} {command_keywords}"
 
     ctx.run(command, pty=True)
     if coverage:
-        ctx.run(f"{DJANGO_EXEC} coverage combine; {DJANGO_EXEC} coverage report --include='{'*,'.join(roots)}*'", pty=True)
+        ctx.run(
+            f"{DJANGO_EXEC} coverage combine; {DJANGO_EXEC} coverage report --include='{'/*,'.join(roots)}/*'", pty=True
+        )
 
 
-@task(help={
-    'merge': 'If the migrations should be merged into one file',
-})
+@task(
+    help={
+        "merge": "If the migrations should be merged into one file",
+    }
+)
 def makemigrations(ctx, merge=False, empty=False):
     """Call the django makemigrations command"""
 
     if merge:
-        command(ctx, 'makemigrations --merge')
+        command(ctx, "makemigrations --merge")
     else:
-        command(ctx, 'makemigrations')
+        command(ctx, "makemigrations")
 
 
 @task
 def showmigrations(ctx):
     """Call the django makemigrations command"""
-    command(ctx, 'showmigrations')
+    command(ctx, "showmigrations")
 
 
 @task
@@ -202,20 +242,32 @@ def sqlmigrate(ctx, migration):
     Call the django sqlmigrate command to show
     the SQL code that will correspond to a given migration.
     """
-    command(ctx, f'sqlmigrate {migration}')
+    command(ctx, f"sqlmigrate {migration}")
 
 
-@task(help={'migration': 'Which migration should be applied/unapplied. e.g. "inv migrate -m \'reports 00145\'"'})
+@task(help={"migration": "Which migration should be applied/unapplied. e.g. \"inv migrate -m 'clinic 00145'\""})
 def migrate(ctx, migration=""):
     """Call the django migrate command"""
-    command(ctx, f'migrate {migration}')
+    command(ctx, f"migrate {migration}")
 
 
 @task
-def bash(ctx, service='django', rest='', hide=False):
+def makemessages(ctx):
+    """Update the messages file"""
+    ctx.run("django-admin makemessages -l en --ignore venv -e py")
+
+
+@task
+def bash(ctx, service="django", rest="", hide=False):
     """Invoke bash in the specified service. Django by default"""
-    command = f'-c "{rest}"' if rest else ''
+    command = f'-c "{rest}"' if rest else ""
     ctx.run(f"docker compose exec {service} bash {command}", pty=True, hide=hide)
+
+
+@task
+def compilemessages(ctx):
+    """Update the messages file"""
+    ctx.run("django-admin compilemessages --ignore venv")
 
 
 def copy_file(file, destination):
@@ -223,7 +275,7 @@ def copy_file(file, destination):
     try:
         shutil.copy(file, Path(destination) / Path(file).name)
     except FileExistsError:
-        print(f'File {file} already exists in {destination}. Overwriting...')
+        print(f"File {file} already exists in {destination}. Overwriting...")
         Path(destination).joinpath(Path(file).name).unlink()
         copy_file(file, destination)
 
@@ -233,7 +285,7 @@ def copy_folder(folder, destination):
     try:
         shutil.copytree(folder, Path(destination) / Path(folder).name)
     except FileExistsError:
-        print(f'Folder {folder} already exists in {destination}. Overwriting...')
+        print(f"Folder {folder} already exists in {destination}. Overwriting...")
         shutil.rmtree(Path(destination) / Path(folder).name)
         copy_folder(folder, destination)
 
@@ -247,14 +299,16 @@ def check_project_ownership(path):
         for child in path.iterdir():
             check_project_ownership(child)
     else:
-        if Path(path).stat().st_uid != os.getuid():
+        file = Path(path)
+        if not file.stat().st_uid == os.getuid() and file.is_symlink() is False:
+            print(f"File {path} is not owned by the user running the script.")
             raise Exception
 
 
 @task
 def clean(ctx):
     """
-    Cleans and rebuild the whole environment to make it lighter.
+    Cleans and rebuild the whole environment to make it light and PHI-free.
     Backups the files and folders specified in the CLEAN_ENV.BK_FILES and CLEAN_ENV.BK_FOLDERS
     variables (see the invoke.yaml file).
     """
@@ -263,56 +317,78 @@ def clean(ctx):
     try:
         check_project_ownership(project_root)
     except Exception:
-        print(f'There are files that aren\'t owned by the user running the script, run `sudo chown -R $USER:$USER {project_root}` and try again.')
+        print(
+            f"There are files that aren't owned by the user running the script, run `sudo chown -R $USER {project_root}` and try again."
+        )
         return
 
-    BACK_DATE_FORMAT = r'%m-%d-%yT%H%M%S'
-    backup_dir = ctx.CLEAN_ENV.BAK_DIR if ctx.CLEAN_ENV.BAK_DIR[-1] == '/' else ctx.CLEAN_ENV.BAK_DIR + '/'
+    BACK_DATE_FORMAT = r"%m-%d-%yT%H%M%S"
+    backup_dir = ctx.CLEAN_ENV.BAK_DIR if ctx.CLEAN_ENV.BAK_DIR[-1] == "/" else ctx.CLEAN_ENV.BAK_DIR + "/"
     folder_name = datetime.now().strftime(BACK_DATE_FORMAT)
     backup_folder_name = f"{backup_dir}{folder_name}/"
     Path(backup_folder_name).mkdir(parents=True, exist_ok=True)
 
-    print(f'Backing up to {backup_folder_name}...')
+    print(f"Backing up to {backup_folder_name}...")
     for file in ctx.CLEAN_ENV.BK_FILES:
         copy_file(file, backup_folder_name)
     for folder in ctx.CLEAN_ENV.BK_FOLDERS:
         copy_folder(folder, backup_folder_name)
 
-    print('Cleaning...')
+    print("Cleaning...")
     repo = git.Repo(project_root)
-    repo.git.clean('-fdx')
+    repo.git.clean("-fdx")
 
-    print('Restoring backups...')
+    print("Restoring backups...")
     for file in ctx.CLEAN_ENV.BK_FILES:
         copy_file(backup_folder_name + file, project_root)
     for folder in ctx.CLEAN_ENV.BK_FOLDERS:
         copy_folder(backup_folder_name + folder, project_root)
 
     if ctx.CLEAN_ENV.REMOVE_BACKUP_AFTER_COPY:
-        print('Removing backups...')
+        print("Removing backups...")
         shutil.rmtree(backup_folder_name)
-    print('Done!')
+    print("Done!")
 
 
-@task(help={'all': 'Use the default docker file instead of the dev one'}, pre=[down], post=[db])
-def reset(ctx, all=False):
+@task(pre=[makemessages, compilemessages])
+def translate(ctx):
+    """Run makemessages and compilemessages."""
+    ...
+
+
+@task(pre=[down], post=[db])
+def reset(ctx):
     """Relaunch the docker environment and fill the db with sample data"""
-    up(ctx, all)
+    up(ctx)
+
+
+@task(help={"service": "Service that should be restarted, comma-separated. All services by default"})
+def restart(ctx, service=""):
+    """Restart a service. All services by default."""
+    append = " ".join(service.split(",")) if service else ""
+    ctx.run(f"docker compose restart -t0 {append}", pty=True)
 
 
 @task
 def shell(ctx):
     """Invoke django shell."""
-    command(ctx, 'shell')
+    command(ctx, "shell")
 
 
-@task(help={'script': 'Script that should be runned by Django. Path relative to the django project folder.'})
+@task(help={"script": "Script that should be runned by Django. Path relative to the django project folder."})
 def script(ctx, script):
     """Invoke a django script."""
-    bash(ctx, rest=f"python manage.py shell < {script}")
+    bash(ctx, rest=f"/entrypoint.sh ./manage.py shell < {script}")
+
+
+@task
+def merge(ctx):
+    """Call the merge_patients command"""
+    command(ctx, "merge_patients")
 
 
 @task
 def docker_prune(ctx):
-    """Run the docker system prune command to free up disk space"""
-    ctx.run('docker system prune -af')
+    """Run the docker system prune command to remove all unused images, containers, networks and volumes"""
+    ctx.run("docker system prune -af --volumes")
+    ctx.run("docker container prune -f")
